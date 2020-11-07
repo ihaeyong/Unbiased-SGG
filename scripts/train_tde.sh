93a1a8254dfd9d4cfa32fb43f08df914c52dbfa7
haeyong <ihaeyong@gmail.com>
2020-11-07 13:11:52 +0000

    cheb 1 hop

--- scripts/train_tde.sh
+++ scripts/train_tde.sh
@@ -49,5 +49,5 @@ elif [ $2 == "predcls" ]; then
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/obj_spectrum_ctx1_bias_label-predcls
+           OUTPUT_DIR ./checkpoints/obj_spectrum_ctx1_cheb_K1_bias_label-predcls
 fi
