fed16e965d00ffa7399dd391e8bf86907764d091
haeyong <ihaeyong@gmail.com>
2020-11-08 08:31:38 +0000

    mask gcn

--- maskrcnn_benchmark/modeling/roi_heads/relation_head/model_sgraph_with_context.py
+++ maskrcnn_benchmark/modeling/roi_heads/relation_head/model_sgraph_with_context.py
@@ -43,7 +43,7 @@ class SpectralMessage(nn.Module):
                 nn.ReLU(inplace=True))
 
         # adj. matrix (edges of graph)
-        if False:
+        if True:
             self.adj_matrix = nn.Sequential(
                 nn.Conv2d(51, 10, 3, stride=1, padding=1, bias=False),
                 nn.Conv2d(10,  5, 3, stride=1, padding=1, bias=False),
