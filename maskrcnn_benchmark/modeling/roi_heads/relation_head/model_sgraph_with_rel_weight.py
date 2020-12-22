573844a589b64cde1e9fa77841d675695fa45d8c
haeyong <ihaeyong@gmail.com>
2020-12-22 16:14:26 +0000

    update

--- maskrcnn_benchmark/modeling/roi_heads/relation_head/model_sgraph_with_rel_weight.py
+++ maskrcnn_benchmark/modeling/roi_heads/relation_head/model_sgraph_with_rel_weight.py
@@ -111,26 +111,6 @@ class ObjWeight(nn.Module):
             ent_v = entropy(cls_order, base=151, axis=1).mean()
             skew_v = skew(cls_order, axis=1).mean()
 
-        elif w_type is 'true':
-            batch_freq = freq_bias.data.cpu().numpy()
-            cls_num_list = batch_freq.sum(0)
-            cls_order = batch_freq[:, self.obj_idx]
-
-            ent_v = entropy(cls_order, base=151, axis=1) * topk_true_mask
-            skew_v = skew(cls_order, axis=1) * topk_true_mask
-            ent_v = ent_v.sum() / topk_true_mask.sum()
-            skew_v = skew_v.sum() / topk_true_mask.sum()
-
-        elif w_type is 'false':
-            batch_freq = freq_bias.data.cpu().numpy()
-            cls_num_list = batch_freq.sum(0)
-            cls_order = batch_freq[:, self.obj_idx]
-
-            ent_v = entropy(cls_order, base=151, axis=1) * topk_false_mask
-            skew_v = skew(cls_order, axis=1) * topk_false_mask
-            ent_v = ent_v.sum() / topk_false_mask.sum()
-            skew_v = skew_v.sum() / topk_false_mask.sum()
-
         else:
             cls_order = batch_freq[self.obj_idx]
             ent_v = entropy(cls_order, base=151)
@@ -138,9 +118,9 @@ class ObjWeight(nn.Module):
 
         # skew_v > 0 : more weight in the left tail
         # skew_v < 0 : more weight in the right tail
-        if skew_v > 1.0 :
+        if skew_v > 1.5 :
             beta = 1.0 - ent_v * 1.0
-        elif skew_v < -1.0 :
+        elif skew_v < -1.5 :
             beta = 1.0 - ent_v * 1.0
         else:
             beta = 0.0
