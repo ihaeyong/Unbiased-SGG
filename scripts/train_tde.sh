838952c1f2a9e12b238dc4528fe61e5f65663d0e
haeyong <ihaeyong@gmail.com>
2021-02-12 07:56:40 +0000

    update

--- scripts/train_tde.sh
+++ scripts/train_tde.sh
@@ -3,11 +3,11 @@
 # Train Motifnet using different orderings
 export PYTHONPATH=$HOME/workspaces/unbiased-sg
 export PYTHONIOENCODING=utf-8
-export CUDA_VISIBLE_DEVICES=$3,$4,$5,$6
+export CUDA_VISIBLE_DEVICES=$3,$4
 
 if [ $2 == "sgcls" ]; then
     python -m torch.distributed.launch \
-           --master_port 10050 \
+           --master_port $5 \
            --nproc_per_node=$1 \
            tools/relation_train_net.py \
            --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
@@ -28,7 +28,7 @@ if [ $2 == "sgcls" ]; then
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/mask_subjobj_avg_v0_sig0.04_obj1.9_scale2_sum_v3-sgcls
+           OUTPUT_DIR ./checkpoints/mask_subjobj_avg_v0_sig0.01_obj2.3_scale2_sum_v3-sgcls
 
 elif [ $2 == "predcls" ]; then
     python -m torch.distributed.launch \
