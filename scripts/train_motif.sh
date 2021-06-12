41c82c6b936a7e132907b158f9c2f6114805a9d8
haeyong <ihaeyong@gmail.com>
2021-06-12 11:02:02 +0000

    update

--- scripts/train_motif.sh
+++ scripts/train_motif.sh
@@ -17,7 +17,7 @@ if [ $2 == "sgcls" ]; then
            MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS True \
            MODEL.ROI_RELATION_HEAD.RECT_BOX_EMB True \
            SOLVER.IMS_PER_BATCH 12 \
-           SOLVER.BASE_LR 0.001 \
+           SOLVER.BASE_LR 0.01 \
            TEST.IMS_PER_BATCH $1 \
            DTYPE "float16" \
            SOLVER.MAX_ITER 70000 \
@@ -25,7 +25,7 @@ if [ $2 == "sgcls" ]; then
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/motifs-avg-obj2.1-rel0.9-sgcls
+           OUTPUT_DIR ./checkpoints/motifs-avg-obj2.1-rel1.0-sgcls
 
 elif [ $2 == "predcls" ]; then
     python -m torch.distributed.launch \
@@ -39,14 +39,14 @@ elif [ $2 == "predcls" ]; then
            MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS True \
            MODEL.ROI_RELATION_HEAD.RECT_BOX_EMB True \
            SOLVER.IMS_PER_BATCH 12 \
-           SOLVER.BASE_LR 0.001 \
+           SOLVER.BASE_LR 0.01 \
            TEST.IMS_PER_BATCH $1 \
            DTYPE "float16" SOLVER.MAX_ITER 70000 \
            SOLVER.VAL_PERIOD 2000 \
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/motifs-avg-obj2.1-rel0.9-predcls
+           OUTPUT_DIR ./checkpoints/motifs-avg-obj2.1-rel1.0-predcls
 
 elif [ $2 == "sgdet" ]; then
     python -m torch.distributed.launch \
@@ -60,13 +60,13 @@ elif [ $2 == "sgdet" ]; then
            MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS True \
            MODEL.ROI_RELATION_HEAD.RECT_BOX_EMB True \
            SOLVER.IMS_PER_BATCH 8 \
-           SOLVER.BASE_LR 0.001 \
+           SOLVER.BASE_LR 0.01 \
            TEST.IMS_PER_BATCH $1 \
            DTYPE "float16" SOLVER.MAX_ITER 70000 \
            SOLVER.VAL_PERIOD 2000 \
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/motifs-avg-obj2.1-rel0.9-sgdet
+           OUTPUT_DIR ./checkpoints/motifs-avg-obj2.1-rel1.0-sgdet
 
 fi
