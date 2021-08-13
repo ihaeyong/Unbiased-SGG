b175cc194a28e7d011b684f7e50651d134cc67bd
haeyong <ihaeyong@gmail.com>
2021-08-13 10:22:40 +0000

    motifs

--- scripts/train_motif.sh
+++ scripts/train_motif.sh
@@ -5,29 +5,7 @@ export PYTHONPATH=$HOME/workspaces/unbiased-sg
 export PYTHONIOENCODING=utf-8
 export CUDA_VISIBLE_DEVICES=$3,$4
 
-if [ $2 == "sgcls" ]; then
-    python -m torch.distributed.launch \
-           --master_port $5 \
-           --nproc_per_node=$1 \
-           tools/relation_train_net.py \
-           --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
-           MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
-           MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False \
-           MODEL.ROI_RELATION_HEAD.PREDICTOR MotifPredictor \
-           MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS True \
-           MODEL.ROI_RELATION_HEAD.RECT_BOX_EMB True \
-           SOLVER.IMS_PER_BATCH 12 \
-           SOLVER.BASE_LR 0.01 \
-           TEST.IMS_PER_BATCH $1 \
-           DTYPE "float16" \
-           SOLVER.MAX_ITER 70000 \
-           SOLVER.VAL_PERIOD 2000 \
-           SOLVER.CHECKPOINT_PERIOD 2000 \
-           GLOVE_DIR ./datasets/glove \
-           MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/motifs-obj-cls-avg-obj2.6-rel0.9-sgcls
-
-elif [ $2 == "predcls" ]; then
+if [ $2 == "predcls" ]; then
     python -m torch.distributed.launch \
            --master_port $5 \
            --nproc_per_node=$1 \
@@ -39,14 +17,37 @@ elif [ $2 == "predcls" ]; then
            MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS True \
            MODEL.ROI_RELATION_HEAD.RECT_BOX_EMB True \
            SOLVER.IMS_PER_BATCH 12 \
-           SOLVER.BASE_LR 0.0001 \
+           SOLVER.BASE_LR 0.001 \
            TEST.IMS_PER_BATCH $1 \
            DTYPE "float16" SOLVER.MAX_ITER 70000 \
            SOLVER.VAL_PERIOD 2000 \
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/motifs-ent0.2-obj2.2-rel0.9-predcls
+           OUTPUT_DIR ./checkpoints/motifs_embed_v3_lr1e-3_target-skew0.7_0.7_ent0.18-predcls
+
+elif [ $2 == "sgcls" ]; then
+    python -m torch.distributed.launch \
+           --master_port $5 \
+           --nproc_per_node=$1 \
+           tools/relation_train_net.py \
+           --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
+           MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
+           MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False \
+           MODEL.ROI_RELATION_HEAD.PREDICTOR MotifPredictor \
+           MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS True \
+           MODEL.ROI_RELATION_HEAD.RECT_BOX_EMB True \
+           SOLVER.IMS_PER_BATCH 12 \
+           SOLVER.BASE_LR 0.001 \
+           TEST.IMS_PER_BATCH $1 \
+           DTYPE "float16" \
+           SOLVER.MAX_ITER 70000 \
+           SOLVER.VAL_PERIOD 2000 \
+           SOLVER.CHECKPOINT_PERIOD 2000 \
+           GLOVE_DIR ./datasets/glove \
+           MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
+           OUTPUT_DIR ./checkpoints/motifs_embed_v3_lr1e-3_target-skew0.7_0.7_ent0.18-sgcls
+    
 
 elif [ $2 == "sgdet" ]; then
     python -m torch.distributed.launch \
@@ -68,6 +69,6 @@ elif [ $2 == "sgdet" ]; then
            SOLVER.CHECKPOINT_PERIOD 2000 \
            GLOVE_DIR ./datasets/glove \
            MODEL.PRETRAINED_DETECTOR_CKPT ./checkpoints/pretrained_faster_rcnn/model_final.pth \
-           OUTPUT_DIR ./checkpoints/motifs-obj-cls-avg-obj2.4-rel0.9-sgdet
+           OUTPUT_DIR ./checkpoints/motifs_embed_v3_lr1e-3_target-skew0.7_0.7_ent0.18-sgdet
 
 fi
